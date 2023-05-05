d934baa224ccc5484918c7febd65254e0abefacf
jarastar <taras.y248@gmail.com>
2023-05-05 23:58:05 +0300

    fix

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,50 +1,46 @@
-import { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { fetchMovies } from 'services/api';
+import { useEffect, useState } from 'react';
+import { useSearchParams, useLocation } from 'react-router-dom';
 import MoviesList from 'components/MoviesList/MoviesList';
 import SearchBar from 'components/SearchBar/SearchBar';
+import { fetchMovies } from 'services/api';
 
 const Movies = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
+const location = useLocation();
+const [search, setSearch] = useState('');
+const [movies, setMovies] = useState([]);
+const [error, setError] = useState();
+const [searchParams, setSearchParams] = useSearchParams();
 
-  const [search, setSearch] = useState('');
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const query = params.get('query');
-
-    if (query) {
-      setSearch(query);
-      fetchMovies(query)
-        .then(({ results }) => setMovies(results))
-        .catch(error => setError(error));
-    }
-  }, [location.search]);
+useEffect(() => {
+  const query = searchParams.get('query');
+  if (query) {
+    setSearch(query);
+    fetchMovies(query)
+    .then(({ results }) => setMovies(results))
+    .catch(error => setError(error));
+}
+}, [searchParams, location]);
 
   const onMovieSearch = search => {
     setSearch(search);
     setMovies([]);
-    const queryParams = new URLSearchParams({ query: search });
-    navigate({ search: queryParams.toString() });
-    };
+    setSearchParams({ query: search });
+};
 
   return (
     <main>
-      <SearchBar onSubmit={onMovieSearch} initialValue={search} />
-      {error && <p>{error}</p>}
-      {search && movies.length === 0 && (
-        <p>
-          We do not find any movie with such name. Please, try another keyword!
-        </p>
-      )}
-      {search && movies.length > 0 && (
-        <MoviesList movies={movies} search={search} />
-      )}
+       <SearchBar onSubmit={onMovieSearch} initialValue={search} />
+    {error && <p>{error}</p>}
+    {search && movies.length === 0 && (
+      <p>
+    We did not find any movie with such name. Please, try another keyword!
+    </p>
+    )}
+    {search && movies.length > 0 && (
+    <MoviesList movies={movies} search={search} />
+    )}
     </main>
-  );
+);
 };
 
 export default Movies;
